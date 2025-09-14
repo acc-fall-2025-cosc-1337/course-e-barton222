d841bdf03e116344307cb097e301fa19bd70e165
e-barton222 <ethan.barton@g.austincc.edu>
2025-09-14 00:34:33 -0500

    Update main.cpp

--- src/homework/01_data_types/main.cpp
+++ src/homework/01_data_types/main.cpp
@@ -1,12 +1,9 @@
-﻿//write include statements
+#include <iostream>
+#include "data_types.h"
 
-
-//write namespace using statement for cout
-
-
-int main()
-{
-	
-
-	return 0;
+int main() {
+    std::cout << "7 x 7 = " << multiply_numbers(7, 7) << "\n";
+    std::cout << "5 x 5 = " << multiply_numbers(5, 5) << "\n";
+    return 0;
 }
+
