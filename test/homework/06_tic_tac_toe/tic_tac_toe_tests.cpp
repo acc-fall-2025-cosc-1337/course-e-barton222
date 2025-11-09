6e793be04f67dc3dec25a86f442a9d3875a5095f
e-barton222 <ethan.barton@g.austincc.edu>
2025-11-08 23:34:23 -0600

    Update tic_tac_toe_tests.cpp

--- test/homework/06_tic_tac_toe/tic_tac_toe_tests.cpp
+++ test/homework/06_tic_tac_toe/tic_tac_toe_tests.cpp
@@ -1,7 +1,55 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#define CATCH_CONFIG_MAIN
 #include "catch.hpp"
+#include "tic_tac_toe.h"
 
-TEST_CASE("Verify Test Configuration", "verification") {
-	REQUIRE(true == true);
+// Test first player
+TEST_CASE("Test first player set to X")
+{
+    TicTacToe game;
+    game.start_game("X");
+    REQUIRE(game.get_player() == "X");
 }
 
+TEST_CASE("Test first player set to O")
+{
+    TicTacToe game;
+    game.start_game("O");
+    REQUIRE(game.get_player() == "O");
+}
+
+// Tie game
+TEST_CASE("Test tie game")
+{
+    TicTacToe game;
+    game.start_game("X");
+
+    // Sequence leading to a tie
+    game.mark_board(1);
+    game.mark_board(2);
+    game.mark_board(3);
+    game.mark_board(5);
+    game.mark_board(4);
+    game.mark_board(6);
+    game.mark_board(8);
+    game.mark_board(7);
+    game.mark_board(9);
+
+    REQUIRE(game.game_over() == true);
+    REQUIRE(game.get_winner() == "C");
+}
+
+// Example: Win by first column
+TEST_CASE("Test win by first column")
+{
+    TicTacToe game;
+    game.start_game("X");
+
+    game.mark_board(1);
+    game.mark_board(2);
+    game.mark_board(4);
+    game.mark_board(5);
+    game.mark_board(7);
+
+    REQUIRE(game.game_over() == true);
+    REQUIRE(game.get_winner() == "X");
+}
